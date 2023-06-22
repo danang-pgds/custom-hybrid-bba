f4064421157b9f5ecc627a59d47ac20138620ba5
danang-pgds <danang.pgds@gmail.com>
2023-06-22 22:02:29 +0800

    Update calendar.js
    - convert calendar to vue arch

--- src/calendar.js
+++ src/calendar.js
@@ -1,264 +1,224 @@
 'use strict';
-
-var date = new Date();
-var d = date.getDate();
-var m = date.getMonth();
-var y = date.getFullYear();
-
-var dd = d < 10 ? '0' + d:d;
-var mm = m+1 < 10 ? '0' + (m+1):m+1;
-const monthLong = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-const daysShort = ['Mo','Tu','We','Th','Fr','Sa','Su'];
-
-var currentDay = 0;
-var currentYear = y;
-var currentMonth = m;
-var currentDate = 0;
-var daysInMonth = 0;
-
-const ButtonNextCalendar = ()=>{
-  log('btn next')
-  return `
-    <div class="w-8 h-8 bg-gray-200 hover:bg-red-500 rounded-full text-lg text-white flex cursor-pointer" @:click="press()">
-      <span class="text-center text-white w-full"> > </span>
-    </div>
-  `
-}
-
-const ButtonPrevCalendar = ()=>{
-  log('btn prev')
-  let tmp_month = currentMonth == 0 ? 11 : parseInt(currentMonth) - 1;
-  let tmp_year = currentMonth == 0 ? parseInt(currentYear) - 1 : currentYear;
-  return `
-    <div class="w-8 h-8 bg-gray-200 hover:bg-red-500 rounded-full text-lg text-white flex cursor-pointer">
-      <span class="text-center text-white w-full"> &lt; </span>
-    </div>
-  `
-}
-
-const LabelDays = ()=>{
-  log('label days')
-  let day = ''
-  daysShort.map((val)=> day += `<div class="bg-red-500 py-1.5 md:py-2 w-auto text-white rounded-xl text-center">${val}</div>`)
-  return `
-    <div class="grid grid-cols-7 gap-x-1 md:gap-x-5 w-11/12 mb-1">
-      ${day}
-    </div>
-  `
-}
-
-const LabelDates = (dates)=>{
-  log('label dates')
-  // const dates = props.dates;
-  // console.log(dates)
-  // const dates = Array.from(Array(31));// generate dates for slashing purpose
-  let date = '';
-  dates.map((e,key)=> date += `<div class="py-2 md:py-3 w-auto text-red-500 border rounded-xl text-center my-1 hover:cursor-pointer">${e.date == null ? '':e.date}</div>`)
-  return `
-    <div class="grid grid-cols-7 gap-x-1 md:gap-x-5 w-11/12">
-      ${date}
-    </div>
-  `
-}
-
-const Init = () => {
-  console.log(currentMonth)
-  let days = ShowDay();
-  console.log('reloaded')
-  return `
-    <div>
-      <div class="grid grid-cols-12 gap-4">
-        <div class="flex-col col-start-2 col-span-10 border rounded-lg shadow-lg mb-5">
-          <div class="flex justify-center space-x-4 my-5 items-center">
-            ${ButtonPrevCalendar()}
-            <div class="text-lg font-medium">${dd+' '+monthLong[Number(mm)-1]+' '+y} {{title}}</div>
-            ${ButtonNextCalendar()}
-          </div>
-          <div class="flex justify-center">
-            ${LabelDays()}
-          </div>
-          <div class="flex justify-center">
-            ${LabelDates(days)}
-          </div>
-        </div>
-      </div>
-    </div>
-  `
-}
-/**
-* calculate number of days in a particular month
-*/
-const DaysInMonth = (postMonth = null, postYear = null) => {
-  let year = y;
-  let month = m;
-  if (postYear) year = postYear;
-  if (postMonth) month = postMonth;
-  let lastDay = new Date(year, month + 1, 0);
-  return lastDay.getDate();
-}
-/**
-* calculate number of weeks in a particular month
-*/
-const WeeksInMonth = (postMonth = null, postYear = null) => {
-  let year = y;
-  let month = m;
-  if (postYear) year = postYear;
-  if (postMonth) month = postMonth;
-  currentYear = year;
-  currentMonth = month;
-  // currentDate = date;
-  // find number of days in this month
-  daysInMonth = DaysInMonth(month, year);
-  let numOfweeks = Math.floor((daysInMonth % 7 == 0 ? 0 : 1) + (daysInMonth / 7));
-  let monthStartDay = new Date(year, month, 1).getDay();
-  let monthEndingDay = new Date(year, month, daysInMonth).getDay();
-
-  if ((monthEndingDay < monthStartDay) && (monthEndingDay != 0)) numOfweeks++;
-  if (monthStartDay == 0) numOfweeks++;
-  return numOfweeks;
-}
-
-const ShowDay = (postMonth = currentMonth, postYear = currentYear) => {
-  // log(postMonth)
-  // log(postYear)
-  let weeksInMonth = WeeksInMonth(postMonth, postYear);
-  // console.log('weeksInMonth '+weeksInMonth)
-  let content = [];
-  // Create weeks in a month
-  currentDay = 0; // reset
-  for (let i = 0; i < weeksInMonth; i++) {
-    //Create days in a week
-    for (let j = 1; j <= 7; j++) {
-      content.push(ShowDayContents(i * 7 + j));
-    }
-  }
-  // console.log(content)
-  return content;
-}
-
-const ShowDayContents = (cellNumber)=>{
-  let cellContent = null;
-  let price = '';
-  let currency = '';
-  let isAvailable = false;
-  let allotment = '';
-  // log('currentDay '+currentDay)
-  if (currentDay == 0) {
-    let firstDayOfTheWeek = new Date(currentYear, currentMonth, 1).getDay();
-    // console.log('firstDayOfTheWeek '+firstDayOfTheWeek)
-    // log('cellNumber')
-    // log(cellNumber)
-    if (firstDayOfTheWeek == 0) firstDayOfTheWeek = 7;
-    if (cellNumber == firstDayOfTheWeek) currentDay = 1;
-  }
-  if ((currentDay != 0) && (currentDay <= daysInMonth)) {
-    currentDate = currentYear + '-' + currentMonth + '-' + currentDay;
-    let dd = currentDay;
-    let mm = currentMonth + 1;
-    if (mm < 10) {
-      mm = '0' + mm;
-    }
-    if (currentDay < 10) {
-      dd = '0' + currentDay;
-    }
-    // let current_date = currentYear + '-' + mm + '-' + dd;
-
-    // let cal = DATA.calendar[0].calendar[current_date];
-    // //console.log(cal)
-    // if (!isEmpty(cal)) {
-    //   isAvailable = cal.isAvailable;
-    //   if (isAvailable == true) {
-    //     currency = cal.currency;
-    //     price = cal.rate;
-    //     allotment = cal.allotment;
-    //   }
-    // }
-    cellContent = currentDay;
-    currentDay++;
-  }
-
-  // let today_day = d;
-  // let today_mon = m;
-  // let today_year = y;
-
-  // let time_current_date = new Date(currentYear, currentMonth, currentDay).getTime();
-
-  // let main_tour_date = based_on_main_tour_date;
-  // let based_on_current_date = based_on_current_date;
-  // //console.log(time_current_date +' < '+ based_on_current_date)
-  // let pub_time_current_date = new Date(y, m, d).getTime();
-
-  // if (cellContent == today_day && me.currentMonth == today_mon && me.currentYear == today_year) {
-  //   // class_day = "this_today";
-  //   if (!isAvailable) {
-  //     class_day = "this_past";
-  //   } else {
-  //     class_day = "nums_days";
-  //   }
-  // }
-
-  let finalDisplay = {
-    // class: class_day,
-    date: cellContent,
-    // price: price,
-    // currency: currency,
-    // allotment: allotment,
-    // start_time:start_time, 
-    // end_time:end_time
-  };
-
-  return finalDisplay;
-}
-
-const BtnTest = (props)=>{
-  const add = () => {
-    localStorage.setItem("lastname", JSON.stringify({"name":"danang"}));
-    alert('added');
-  }
-  const del = () => {
-    localStorage.removeItem("lastname");
-    alert('deleted');
-  }
-  const get = () => {
-    let a = localStorage.getItem("lastname")
-    alert(a);
-  }
-  const getAction = ()=>{
-    if (props.action == 'add') return add();
-    if (props.action == 'del') return del();
-    if (props.action == 'get') return get();
-  }
-  return `
-    <button onClick={() => getAction()} class="group m-5 rounded-2xl h-12 w-48 bg-green-500 font-bold text-lg text-white relative overflow-hidden">
-        {props.text}
-        <div class="absolute duration-300 inset-0 w-full h-full transition-all scale-0 group-hover:scale-100 group-hover:bg-white/30 rounded-2xl">
-        </div>
-    </button>
-  `
-}
-
-const log = (text)=> console.log(text)
+import { nextTick } from 'vue'
 
 export default {
   props: ['title'],
   data() {
-    return { count: 0 }
-  },
-  mounted: (dt)=>{
-    // console.log(dt)
-  },
-  methods: {
-    press: function () {
-      this.$emit('inCount',1)
-      // log(this.title)
-      // this.title += ' is my name';
-      currentMonth += 1;
-      if(currentMonth > 12) {currentMonth = 1;currentYear++}
-      log(currentMonth)
-      log(currentYear)
-      date = new Date(currentYear+'-'+currentMonth+'-'+d)
-      log(date)
+    return { 
+      dates: '',
+      date: '',
+      d: '',
+      m: '',
+      y: '',
+      monthLong: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      daysShort: ['Mo','Tu','We','Th','Fr','Sa','Su'],
+      dd: '',
+      mm: '',
+      currentDay: 0,
+      currentYear: 0,
+      currentMonth: 0,
+      currentDate: 0,
+      daysInMonth: 0,
     }
   },
-  template: Init()
+  created(){
+    console.log('created')
+    console.log(new Date().getMilliseconds())
+
+    let date = new Date();
+    this.date = date;
+    this.d = date.getDate();
+    this.m = date.getMonth();
+    this.y = date.getFullYear();
+    this.dd = this.d < 10 ? '0' + this.d:this.d;
+    this.mm = this.m+1 < 10 ? '0' + (this.m+1):this.m+1,
+    this.currentMonth = Number(this.mm);
+    this.currentYear = this.y;
+    
+    console.log(this.date)
+  },
+  mounted(){
+    console.log('mounted')
+    nextTick(()=>{
+      console.log('inside nextick')
+      console.log(new Date().getMilliseconds())
+      // this.nextMonth()
+    })
+    console.log('mounted 1')
+    console.log(new Date().getMilliseconds())
+    // this.d < 10 ? '0' + this.d:this.d;
+  },
+  computed: {
+    // compute caching https://vuejs.org/guide/essentials/computed.html#computed-properties
+    LabelDays(){
+      console.log('label days')
+      let day = '';
+      this.daysShort.map((val)=> day += `<div class="bg-red-500 py-1.5 md:py-2 w-auto text-white rounded-xl text-center">${val}</div>`)
+      return day;
+    },
+    LabelDates(){
+      console.log('label dates')
+      // const dates = Array.from(Array(31));// generate dates for slashing purpose
+      this.dates = this.ShowDay();
+      let date = '';
+      this.dates.map((e,key)=> date += `<div class="py-2 md:py-3 w-auto text-red-500 border rounded-xl text-center my-1 hover:cursor-pointer">${e.date == null ? '':e.date}</div>`)
+      return date;
+    },
+  },
+  methods: {
+    emitExample(){this.$emit('inCount',1)},
+    nextMonth(){
+      this.currentMonth += 1;
+      if(this.currentMonth > 12) {this.currentMonth = 1;this.currentYear++}
+      console.log(this.currentMonth)
+    },
+    ShowDay(){
+      console.log('ShowDay')
+      console.log(this.currentMonth)
+      console.log(this.currentYear)
+      let weeksInMonth = this.WeeksInMonth(this.currentMonth, this.currentYear);
+      let content = [];
+      // Create weeks in a month
+      this.currentDay = 0; // reset
+      for (let i = 0; i < weeksInMonth; i++) {
+        //Create days in a week
+        for (let j = 1; j <= 7; j++) {
+          content.push(this.ShowDayContents(i * 7 + j));
+        }
+      }
+      console.log('content')
+      console.log(content)
+      return content;
+    },
+    WeeksInMonth(postMonth, postYear){// calculate number of weeks in a particular month
+      console.log('WeeksInMonth')
+      console.log(postMonth)
+      console.log(postYear)
+      // find number of days in this month
+      this.daysInMonth = this.DaysInMonth(postMonth, postYear);
+      let numOfweeks = Math.floor((this.daysInMonth % 7 == 0 ? 0 : 1) + (this.daysInMonth / 7));
+      let monthStartDay = new Date(postYear, postMonth, 1).getDay();
+      let monthEndingDay = new Date(postYear, postMonth, this.daysInMonth).getDay();
+      console.log('monthStartDay')
+      console.log(this.daysInMonth)
+      console.log(numOfweeks)
+      console.log(monthStartDay)
+      console.log(monthEndingDay)
+      if ((monthEndingDay < monthStartDay) && (monthEndingDay != 0)) numOfweeks++;
+      if (monthStartDay == 0) numOfweeks++;
+      return numOfweeks;
+    },
+    DaysInMonth(postMonth, postYear){ //calculate number of days in a particular month
+      let lastDay = new Date(postYear, postMonth, 0);
+      // console.log('lastDay')
+      // console.log(lastDay.getDate())
+      return lastDay.getDate();
+    },
+    ShowDayContents(cellNumber){
+      let cellContent = null;
+      let price = '';
+      let currency = '';
+      let isAvailable = false;
+      let allotment = '';
+      // log('currentDay '+this.currentDay)
+      if (this.currentDay == 0) {
+        console.log('currentDay')
+        console.log(this.currentDay)
+        let firstDayOfTheWeek = new Date(this.currentYear, this.currentMonth, 1).getDay();
+        console.log('firstDayOfTheWeek '+firstDayOfTheWeek)
+        console.log('cellNumber')
+        console.log(cellNumber)
+        if (firstDayOfTheWeek == 0) firstDayOfTheWeek = 7;
+        if (cellNumber == firstDayOfTheWeek) this.currentDay = 1;
+      }
+      if ((this.currentDay != 0) && (this.currentDay <= this.daysInMonth)) {
+        this.currentDate = this.currentYear + '-' + this.currentMonth + '-' + this.currentDay;
+        let dd = this.currentDay;
+        let mm = this.currentMonth + 1;
+        if (mm < 10) {
+          mm = '0' + mm;
+        }
+        if (this.currentDay < 10) {
+          dd = '0' + this.currentDay;
+        }
+        // let current_date = currentYear + '-' + mm + '-' + dd;
+    
+        // let cal = DATA.calendar[0].calendar[current_date];
+        // //console.log(cal)
+        // if (!isEmpty(cal)) {
+        //   isAvailable = cal.isAvailable;
+        //   if (isAvailable == true) {
+        //     currency = cal.currency;
+        //     price = cal.rate;
+        //     allotment = cal.allotment;
+        //   }
+        // }
+        cellContent = this.currentDay;
+        this.currentDay++;
+      }
+    
+      // let today_day = d;
+      // let today_mon = m;
+      // let today_year = y;
+    
+      // let time_current_date = new Date(currentYear, currentMonth, currentDay).getTime();
+    
+      // let main_tour_date = based_on_main_tour_date;
+      // let based_on_current_date = based_on_current_date;
+      // //console.log(time_current_date +' < '+ based_on_current_date)
+      // let pub_time_current_date = new Date(y, m, d).getTime();
+    
+      // if (cellContent == today_day && me.currentMonth == today_mon && me.currentYear == today_year) {
+      //   // class_day = "this_today";
+      //   if (!isAvailable) {
+      //     class_day = "this_past";
+      //   } else {
+      //     class_day = "nums_days";
+      //   }
+      // }
+    
+      let finalDisplay = {
+        // class: class_day,
+        date: cellContent,
+        // price: price,
+        // currency: currency,
+        // allotment: allotment,
+        // start_time:start_time, 
+        // end_time:end_time
+      };
+    
+      return finalDisplay;
+    },
+    ButtonNextCalendar(){
+      console.log('btn next clicked')
+    },
+    ButtonPrevCalendar(){
+      log('btn prev clicked')
+      let tmp_month = this.currentMonth == 0 ? 11 : parseInt(this.currentMonth) - 1;
+      let tmp_year = this.currentMonth == 0 ? parseInt(this.currentYear) - 1 : this.currentYear;
+    }
+  },
+  template: `
+  <div>
+    <div class="grid grid-cols-12 gap-4">
+      <div class="flex-col col-start-2 col-span-10 border rounded-lg shadow-lg mb-5">
+        <div class="flex justify-center space-x-4 my-5 items-center">
+          <div class="w-8 h-8 bg-gray-200 hover:bg-red-500 rounded-full text-lg text-white flex cursor-pointer">
+            <span class="text-center text-white w-full"> &lt; </span>
+          </div>
+          <div class="text-lg font-medium">{{dd}} {{monthLong[Number(currentMonth-1)]}} {{y}}</div>
+          <div class="w-8 h-8 bg-gray-200 hover:bg-red-500 rounded-full text-lg text-white flex cursor-pointer" @:click="nextMonth()">
+            <span class="text-center text-white w-full"> > </span>
+          </div>
+        </div>
+        <div class="flex justify-center">
+          <div class="grid grid-cols-7 gap-x-1 md:gap-x-5 w-11/12 mb-1" v-html="LabelDays"></div>
+        </div>
+        <div class="flex justify-center">
+          <div class="grid grid-cols-7 gap-x-1 md:gap-x-5 w-11/12" v-html="LabelDates"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
 }
